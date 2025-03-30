76999c45088235d13ef4d1fe98e3cf1ecb73ef0b
ashlym1 <ashlym3@usf.edu>
2025-03-30 11:46:28 -0400

    Add fetchProductsAsync using async/await and try/catch

--- cc_16.js
+++ cc_16.js
@@ -1,3 +1,5 @@
+// Task 1: setting up the project structure : created folder,and files :) 
+
 // Task 2 : fetch products with .then()
 // **trial** trying to fetch the product data from the link 
 function fetchProductsThen() {
@@ -20,3 +22,26 @@ function fetchProductsThen() {
     console.log("Fetch eror",error );
     });
 }
+
+// Task 3: Fetch Products with async/await 
+// this is the same as before but now it's usifn async
+async function fetchProductAsync() {
+    // APU url for the products 
+ const url = "https://www.course-api.com/javascript-store-products";
+
+ try{
+    // pausing; this will waiting for the fetch to finish 
+    const response = await fetch (url);
+    //The reasponse will turn into product data 
+    const products = await response.json();
+
+    // calling the functions to display the products on the page 
+    displayProducts(products)
+ } catch (error) {
+    //  this serves incase of an error; it should call the error handler 
+    handleError(error);
+
+ }
+ }
+
+
